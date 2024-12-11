77ff02c59f1800c3f4e8c2b2b1d5b57433e727da
mohamed-yassine2904 <yassine2904@gmail.com>
2024-12-11 14:21:48 +0100

    to push

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const userroute=require('./routes/user')
+
 
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3306;
+const PORT = 5000;
+app.use("/user" , userroute)
 
 
 
