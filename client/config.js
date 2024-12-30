739eec0bf589172b4ce1dc6ffaa954608e2d25f2
FatmaChirchi <chirchifatma@gmail.com>
2024-12-30 10:45:02 +0100

    push all chat and notifications

--- client/config.js
+++ client/config.js
@@ -1,6 +1,5 @@
-const DEV_URL = 'http://localhost:5000';
-const PROD_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-
+const DEV_URL = 'http://192.168.1.20:5000';
+const PROD_URL = 'http://192.168.1.20:5000' 
 export const getBaseUrl = () => {
     return __DEV__ ? DEV_URL : PROD_URL;
   };
